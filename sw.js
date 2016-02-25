4286f3696e3dce7e79c2bb7ba17db0372bac207c
keshavuser <wpkeshavprogramer@gmail.com>
2016-02-25 13:14:36 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
 
+var CACHE_NAME = 'my-site-cache-v1';
+    var urlsToCache = [
+      'styles/main.css',
+      'script/main.js'
+    ];
+
+    self.addEventListener('install', function(event) {
+      // Perform install steps
+      event.waitUntil(
+        caches.open(CACHE_NAME)
+          .then(function(cache) {
+            alert('Opened cache');
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+          })
+      );
+    });
